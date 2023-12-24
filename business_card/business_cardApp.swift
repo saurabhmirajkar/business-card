69c082d8fc0b850c3b688739817bb9979e7c094d
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-12-24 13:10:12 +0530

    Initial Commit

---
+++ business_card/business_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  business_cardApp.swift
+//  business_card
+//
+//  Created by Saurabh Mirajkar on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct business_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
